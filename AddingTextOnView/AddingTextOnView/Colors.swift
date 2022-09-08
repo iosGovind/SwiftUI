72e85acde1f16d7e3f4fbb7db01172b04295919f
iOSGovind <govind.solanki@appinventiv.com>
2022-09-08 08:50:17 +0530

    Day 3 Colors and Shadows

---
+++ AddingTextOnView/AddingTextOnView/Colors.swift
@@ -0,0 +1,20 @@
+//
+//  Colors.swift
+//  AddingTextOnView
+//
+//  Created by Govind Solanki on 2022-09-07.
+//
+
+import SwiftUI
+
+struct Colors: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Colors_Previews: PreviewProvider {
+    static var previews: some View {
+        Colors()
+    }
+}
